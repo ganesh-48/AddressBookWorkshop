edfc9247fe7b17314a089d269a706f73b93e284f
ganesh-48 <ganeshsrananavare@gmail.com>
2021-05-04 09:18:36 +0530

    ADDED: Created a contact in address book

--- src/main/java/com/addressbook/AddressBook.java
+++ src/main/java/com/addressbook/AddressBook.java
@@ -1,7 +1,13 @@
 package com.addressbook;
 
+/*@Description-Creating a Address Book.
+* Welcome to address book.
+* Contacts are created. */
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println("Welcome to address book");
+
+        Contacts contact = new Contacts("Ram", "Sharma", "Line - 3", "maharashtra", "solapur", 123456, 1234567899, "abc@gmail.com");
+        System.out.println(contact.toString());
     }
 }
