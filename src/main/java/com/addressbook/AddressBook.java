4df962239406575fc272c5a9c6c5d900afbdaecd
ganesh-48 <ganeshsrananavare@gmail.com>
2021-05-10 00:02:34 +0530

    ADDED: Read or write address book with person contact as JSON file

--- src/main/java/com/addressbook/AddressBook.java
+++ src/main/java/com/addressbook/AddressBook.java
@@ -1,9 +1,17 @@
 package com.addressbook;
 
+import com.google.gson.*;
+import com.google.gson.GsonBuilder;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+;
 
 /*@Description-Creating a Address Book.
 * Welcome to address book.
@@ -16,93 +24,60 @@ import java.util.*;
 * Return the data contact list.
 * Check a duplicate entry of same person in address book.
 * Search person by city.
-* Search person by state.*/
+* Search person by state.
+* Read or write a  address book with person contact as JSON file.*/
 
 public class AddressBook {
 
-    public static void writeCsv(String filePath) {
-        List<Contacts> contacts = new ArrayList<Contacts>();
-        Contacts contactDataList = new Contacts(filePath, filePath, filePath, filePath, filePath, filePath, filePath, filePath, filePath);
-        contactDataList.setFirstName("Ram");
-        contactDataList.setLastName("Sharma");
-        contactDataList.setAddress("Line-3");
-        contactDataList.setCity("Solapur");
-        contactDataList.setState("Maharashtra");
-        contactDataList.setZip(123456);
-        contactDataList.setPhoneNumber(1234567899L);
-        contactDataList.setEmail("abc@gmail.com");
-        contacts.add(contacts);
-        FileWriter fileWriter = null;
-        try {
-            fileWriter = new FileWriter(filePath);
-            fileWriter.append("firstName,lastName,address,city,state,zip,phoneNumber,Email");
-            for (Contacts contacts : contacts) {
-                fileWriter.append(String.valueOf(contacts.getFirstName()));
-                fileWriter.append(String.valueOf(contacts.getLastName()));
-                fileWriter.append(String.valueOf(contacts.getAddress()));
-                fileWriter.append(String.valueOf(contacts.getCity()));
-                fileWriter.append(String.valueOf(contacts.getState()));
-                fileWriter.append(String.valueOf(contacts.getZip()));
-                fileWriter.append(String.valueOf(contacts.getPhoneNumber()));
-                fileWriter.append(String.valueOf(contacts.getEmail()));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                fileWriter.flush();
-                fileWriter.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
+    private static final String HOME = System.getProperty("user.dir");
+    private static final String fileName = "AddressBook.json";
+    private static final Path homePath = Paths.get(HOME);
+    public static final Gson gson = new GsonBuilder().create();
 
-    public static void readCsv(String filePath) {
-        BufferedReader reader = null;
-        try {
-            List<Contacts> employeeContacts = new ArrayList<Contacts>();
-            String line = "";
-            reader = new BufferedReader(new FileReader(filePath));
-            reader.readLine();
-            while (( reader.readLine() != null)) {
-                String[] field = line.split(",");
-                if (!(field.length() <= 0)) {
-                    Contacts contacts = new Contacts(field, field, field, field, field, field, field, field, field);
-                    contacts.setFirstName("Prahlad");
-                    contacts.setLastName("Prajapati");
-                    contacts.setAddress("Azamgarh");
-                    contacts.setCity("Azamgarh");
-                    contacts.setState("up");
-                    contacts.setZip(223223);
-                    contacts.setPhoneNumber(8052636931l);
-                    contacts.setEmail("amarprajapati99@gmail.com");
-                    contacts.add(contacts);
+    /* @Description - To write  the contacts details. */
+    public static boolean jsonWrite(Contacts contacts) {
+        if (Files.exists(homePath)) {
+            Path filePath = Paths.get(HOME + "/" + fileName);
+            try {
+                if (Files.exists(filePath)) {
+                    String s = gson.toJson(contacts);
+                    FileWriter fileWriter = new FileWriter(String.valueOf(filePath));
+                    fileWriter.write(s);
+                    fileWriter.close();
+                    return true;
+                } else {
+                    Files.createFile(filePath);
+                    String s = gson.toJson(contacts);
+                    FileWriter fileWriter = new FileWriter(String.valueOf(filePath));
+                    fileWriter.write(s);
+                    fileWriter.close();
+                    return true;
                 }
-            }
-            for (Contacts addressBook : employeeContacts) {
-                System.out.printf(
-                        "First Name = %s,Last Name = s, Address = %s, City = %s, State = %s,Zip = %s,Phone Number = %s,Email = %s",
-                        addressBook.getFirstName(), addressBook.getLastName(), addressBook.getAddress(), addressBook.getCity(), addressBook.getState(), addressBook.getZip(),
-                        addressBook.getPhoneNumber(), addressBook.getEmail());
-            }
-        } catch (Exception e) {
-            e.fillInStackTrace();
-        } finally {
-            try {
-                reader.close();
-            } catch (Exception e) {
-                e.printStackTrace();
+            } catch (IOException e) {
+                return false;
             }
         }
+        return true;
+    }
+    /* @Description - To read the contacts details. */
+
+    public static boolean jsonRead() {
+        if (Files.exists(homePath)) {
+            Path filePath = Paths.get(HOME + "/" + fileName);
+            try {
+                if (Files.exists(filePath)) {
+                    BufferedReader br = new BufferedReader(
+                            new FileReader(String.valueOf(filePath)));
+                    Contacts contactPerson = gson.fromJson(br, Contacts.class);
+                    System.out.println("ContactAddressBookPerson{" + "firstName='" + contactPerson.firstName + '\'' + ", lastName='" + contactPerson.lastName + '\'' + ", address='" + contactPerson.address + '\'' + ", city='" + contactPerson.city + '\'' + ", state='" + contactPerson.state + '\'' + ", zip=" + contactPerson.zip + ", mobileNumber='" + contactPerson.phoneNumber + '\'' + ", email='" + contactPerson.email + '\'' + '}' );
+                    return true;
+                }
+            } catch (IOException e) {
+                return false;
+            }
+        }
+        return true;
+
     }
 
-    public static void main(String[] args) {
-        String filePath = "C:\\Users\\Ganesh\\CsvReader\\AddressBookIO";
-        System.out.println("Starting File writing :" + filePath);
-        writeCsv(filePath);
-        System.out.println("starting read file");
-        readCsv(filePath);
-
-    }
 }
--- src/test/java/com/addressbook/AddressBookTest.java
+++ src/test/java/com/addressbook/AddressBookTest.java
@@ -1,44 +1,15 @@
 package com.addressbook;
 
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.IntStream;
 
 public class AddressBookTest {
-    private static String HOME = System.getProperty("C:\\Users\\Ganesh\\CsvReader\\AddressBookIO");
-    private static String checkContacts = "Contact Details";
+
+    AddressBook addressBook = new AddressBook();
 
     @Test
-    public void givenPathWhenCheckedThenConfirmTrue() throws IOException {
-        System.out.println(HOME);
-        Path homePath = Paths.get(HOME);
-        Assertions.assertTrue(Files.exists(homePath));
-
-        Path path = Paths.get(HOME + "/" + checkContacts);
-        if (Files.exists(path)) Files.delete(path);
-        Assertions.assertTrue(Files.notExists(path));
-        Files.createDirectories(path);
-        Assertions.assertTrue(Files.exists(path));
-        IntStream.range(1, 10).forEach(cntr -> {
-            Path tempFile = Paths.get(path + "/temp" + cntr);
-            Assertions.assertTrue(Files.notExists(tempFile));
-            try {
-                Files.createFile(tempFile);
-            } catch (IOException e) {
-            }
-            Assertions.assertTrue(Files.exists(tempFile));
-        });
-
-        Files.list(path).filter(Files::isRegularFile).forEach(System.out::println);
-        Files.newDirectoryStream(path).forEach(System.out::println);
-        Files.newDirectoryStream(path, path1 -> path.toFile().isFile() && path
-                .toString().startsWith("temp")).forEach(System.out::println);
+    public void givenPathWhenCheckedThenConfirmTrue() {
+        addressBook = new AddressBook();
     }
-
-
 }
